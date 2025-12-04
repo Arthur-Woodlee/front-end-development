2725bd1c30221b1be9d076a4c5848d3dc7326310
Arthur-Woodlee <adamwoodlee136@icloud.com>
2025-12-04 17:24:05 +1100

    Add SCSS loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,28 +15,42 @@
 // - __dirname: absolute path to the directory containing the current file.
 //
 // These are passed in automatically and scoped per file.
-// __dirname is especially useful for resolving paths (e.g., output directory in Webpack). 
-const path = require('path');// Imports Node.js's built-in 'path' module to safely resolve and manipulate file system paths.
-const HtmlWebpackPlugin = require('html-webpack-plugin');// (3rd Party plugin)Imports the HtmlWebpackPlugin, which generates an HTML file and automatically injects bundled JS and CSS assets.
+// __dirname is especially useful for resolving paths (e.g., output directory in Webpack).
+const path = require("path"); // Imports Node.js's built-in 'path' module to safely resolve and manipulate file system paths.
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // (3rd Party plugin)Imports the HtmlWebpackPlugin, which generates an HTML file and automatically injects bundled JS and CSS assets.
 
 module.exports = {
-  entry: './src/entry/index.js', // main entry point. Builds dependency graph following import and require statements
+  entry: {
+    index: "./src/entry/index.js",
+  }, // main entry point. Builds dependency graph following import and require statements
   output: {
-    filename: '[name].[contenthash].js', // Names each output file using its entry name and a content-based hash for cache busting.
-    path: path.resolve(__dirname, 'dist'), // Resolves the absolute path to the 'dist' folder for writing output files.
-    clean: true,// Clears the output directory before each build to remove outdated files.
+    filename: "[name].[contenthash].js", // Names each output file using its entry name and a content-based hash for cache busting.
+    path: path.resolve(__dirname, "dist"), // Resolves the absolute path to the 'dist' folder for writing output files.
+    clean: true, // Clears the output directory before each build to remove outdated files.
   },
-  plugins: [
+  module: {
+    rules: [ // At the ver least this enable webpack to understand import '../styles/pages/index.scss'; in some .js file, and compiles the scss into css.
+      {
+        test: /\.scss$/, // matches .scss files
+        use: [
+          "style-loader", // injects styles into DOM
+          "css-loader", // interprets @import and url()
+          "sass-loader", // compiles SCSS to CSS
+        ],
+      },
+    ],
+  },
+  plugins: [ // At the very least this injects <script> tags into the html
     new HtmlWebpackPlugin({
-        filename: 'index.html', // Omitting this defaults to index.html. Determines the name of the output file.
-        template: './src/pages/index.html', // Specifies which html file to inject into
-        chunks: ['index'] // Controls which bundles get injected into the html.
+      filename: "index.html", // Omitting this defaults to index.html. Determines the name of the output file.
+      template: "./src/pages/index.html", // Specifies which html file to inject into
+      chunks: ["index"], // Controls which bundles get injected into the html.
     }),
   ],
   devServer: {
-    static: './dist',// Tells webpack server to serve from specified directory
+    static: "./dist", // Tells webpack server to serve from specified directory
     hot: true, // HMR
     open: true, // Saves you from manually typing http://localhost:5173 or similar.
   },
-  mode: 'development',
+  mode: "development",
 };
